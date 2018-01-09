bd69a943a23203420c533faf140a043e5f50453d
Alienah <aidalbla@gmail.com>
2018-01-09 23:51:36 +0100

    Change callbacks for promises

--- js/reasons.js
+++ js/reasons.js
@@ -1,23 +1,20 @@
-'use strict';
 function getMoreReasons(){
-	var request;
-	request = new XMLHttpRequest();
-	request.open('GET', 'https://three-random-reasons-izwfjpgbqm.now.sh/');
 
-	request.addEventListener('load', showReasons);
-
-	function showReasons(){
-	  var response = request.responseText;
-	  var reasons = JSON.parse(response).reasons;
-	  var listHTML = '';
-	  for (var i = 0; i < reasons.length; i++) {
-	    listHTML += '<li class="block-text reasons-item"><h3 class="list-title">' + reasons[i].title + '</h3><p>' + reasons[i].description + '</p></li>';
-	  }
-	  var ul = document.querySelector('.reasons-list');
-	  ul.innerHTML += listHTML;
-	}
-
-	request.send();
+fetch('https://three-random-reasons-izwfjpgbqm.now.sh/')
+.then(function(response){
+  return response.json();
+  })
+.then(function(json){
+  console.log(json);
+  var reasons = json.reasons;
+  var listHTML = '';
+	for (var i = 0; i < reasons.length; i++) {
+	  listHTML += '<li class="block-text reasons-item"><h3 class="list-title">' + reasons[i].title + '</h3><p>' + reasons[i].description + '</p></li>';
+  	}
+	var ul = document.querySelector('.reasons-list');
+	ul.innerHTML += listHTML;
+	});
 }
+
 var moreButton = document.querySelector('.more-button');
 moreButton.addEventListener('click', getMoreReasons);
