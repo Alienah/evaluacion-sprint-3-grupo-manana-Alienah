f9195dd7a67ad576f1488286df9494f49d2b612b
Alienah <aidalbla@gmail.com>
2018-01-09 11:00:12 +0100

    Put contain of nav inside a div and change transform way

--- js/main.min.js
+++ js/main.min.js
@@ -1,14 +1,15 @@
 var page = document.querySelector('.page');
+var body = document.querySelector('body');
 var menuOpen = document.querySelector('.navigator__open-button');
 var menuClose = document.querySelector('.navigator__close-button');
 var menuSelector = document.querySelector('.menu');
 
 function openNavigationMenu(){
-	page.classList.add('page__menu-visible');
+	body.classList.add('page__menu-visible');
 };
 
 function closeNavigationMenu(){
-	page.classList.remove('page__menu-visible');
+	body.classList.remove('page__menu-visible');
 };
 
 menuOpen.addEventListener('click', openNavigationMenu);
